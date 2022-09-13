b93eeca68d9973f2681d0166e36079a541822441
SAIZAWMYINT-PC\SaiZawMyint <scm.saizawmyint@gmail.com>
2022-09-13 22:26:42 +0630

    Vue Road Map Project Set up

---
+++ RoadMap/RoadMap/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+import { provide } from 'vue';
+import map from './map/map';
+
+  export default{
+    setup(){
+      provide('map', map)
+    }
+  }
+</script>
---
+++ RoadMap/RoadMap/src/map/map.js
@@ -0,0 +1,11 @@
+import { reactive } from "vue";
+
+const map = reactive({
+    title: "",
+    description: "",
+    link: ""
+});
+
+export default{
+    map
+}
